a9f620ca5ff1d2db1b2716df7db6151e839dce99
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2021-12-13 13:29:16 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,26 @@
-{
-  "docs": [
-    "introduction",
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  notes: [
+    'introduction',
     {
-      "type": "category",
-      "label": "Test",
-      "link": {
-        "type": "generated-index"
+      type: 'category',
+      label: 'test',
+      link: {
+        type: 'generated-index',
       },
-      "collapsed": false,
-      "items": [
-        "Organisations",
-        "Organisation Structure",
-        "Vision and Mission",
-      ]
+      collapsed: false,
+      items: [
+        'Organisations',
+      ],
     },
+
+module.exports = sidebars;
