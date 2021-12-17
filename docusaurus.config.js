6629a43af90cae0f27836189e77543aa482e5135
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2021-12-17 08:04:52 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,12 @@ module.exports = {
           position: 'left',
           label: 'Notes',
         },
+        {
+          href: 'https://github.com/Nayan-Web/Digital-Support-Notes',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
       ],
     },
     footer: {
