9154c7dc0506bc30d95d6a2ae5284ff905680fd3
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-01-06 11:30:44 +0000

    showcase

--- Website/docusaurus.config.js
+++ Website/docusaurus.config.js
@@ -160,6 +160,16 @@ module.exports = {
       },
     ],
     [
+      'ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        // disableInDev: false,
+      },
+    ],
+    [
       '@docusaurus/plugin-pwa',
       {
         id: "pwa",
