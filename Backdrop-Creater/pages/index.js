5f463c8fada546f822086eec166706c459b21eb2
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-01-10 10:57:54 +0000

    gif

--- Backdrop-Creater/pages/index.js
+++ Backdrop-Creater/pages/index.js
@@ -421,7 +421,7 @@ export default function Home() {
               <div className={styles.previewBox}>
                 <h2>
                   <span className={styles.previewBoxTitle}>
-                  <img src="/assets/pulse2.gif" alt="pulse" width="40" height="40"/> Live Preview
+                  <img src="/assets/gifpulse.gif" alt="pulse" width="40" height="40"/> Live Preview
                   </span>
                 </h2>
                 <div
