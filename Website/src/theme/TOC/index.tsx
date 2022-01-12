67a28a4741240b0f8e44ddbc4dd47b3728e68950
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-01-12 09:28:15 +0000

    Update index.tsx

--- Website/src/theme/TOC/index.tsx
+++ Website/src/theme/TOC/index.tsx
@@ -9,7 +9,6 @@ export default function TOC({ toc, editUrl, ...props }) {
 
   return (
     <div className="toc-wrapper">
-      <h2>Contents</h2>
       <OriginalTOC toc={toc} {...props} />
       <EditThisPage editUrl="https://github.com/PatelN123/Digital-Support-Notes/edit/main/Website/" />
     </div>
