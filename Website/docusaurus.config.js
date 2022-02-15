641bca5cce45f75ca43e005c6ba46e4ccbbd39fc
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-15 09:05:00 +0000

    Update docusaurus.config.js

--- Website/docusaurus.config.js
+++ Website/docusaurus.config.js
@@ -146,7 +146,7 @@ const config = {
         height: 51,
         href: 'https://google.com',
       },
-      copyright: `<span style="line-height: 30px;" >Thanks to <strong> <a href="https://netlify.com"> Netlify </a> </strong> for sponsoring us with a free Pro account! </span> <br /> Copyright © ${new Date().getFullYear()} Nayan Patel`,
+      copyright: `<span style="line-height: 30px;" >Thanks to <strong> <a href="https://netlify.com"> Netlify </a> </strong> for sponsoring us with a free Pro account! </span> <br /> Copyright © ${new Date().getFullYear()} Nayan Patel <br /> Coded with ❤️ by <a href="https://nayanpatel.net">Nayan Patel</a>`,
     },
     prism: {
       theme: require('prism-react-renderer/themes/vsLight'),
