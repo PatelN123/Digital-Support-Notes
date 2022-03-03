9ab15dea6163bf736753812be0c26cfd2e39324f
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-03-03 12:34:59 +0000

    Update badge.js

--- Website/static/badge.js
+++ Website/static/badge.js
@@ -1,6 +1,6 @@
 rnw('init', {
     account: 'dsn.releasenotes.io',
-    selector: '.dot__class', // changes the selector to apply the badge and link to
+    selector: '.release-notes-widget', // changes the selector to apply the badge and link to
     title: 'Latest Updates from dsn', // changes the CSS main title
     auto_show_unseen: false, // if enabled, the release notes will auto open if the user has unseen releases
 });
