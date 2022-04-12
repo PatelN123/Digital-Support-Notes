0b5a866b78f54c32663509133ea7f7f24a337b92
Nayan Patel <naypat20051@gmail.com>
2022-04-12 18:33:11 +0000

    Upgrade

--- Website/docusaurus.config.js
+++ Website/docusaurus.config.js
@@ -21,10 +21,6 @@ const pageOptions = {
 module.exports = {
   scripts: [
     {
-      async: true,
-      src: "https://cdn.splitbee.io/sb.js",
-    },
-    {
       src: "https://status.notes.nayanpatel.net/widget/script.js",
     },
   ],
@@ -39,6 +35,10 @@ module.exports = {
   projectName: 'Digital Support Services notes', // Usually your repo name.
   clientModules: [require.resolve('./src/css/tailwind.css')],
   themeConfig: {
+    umami: {
+      websiteid: "67a6f4d2-2b85-4678-8972-d5d43a6216ab",
+      src: "https://analytics.notes.nayanpatel.net/umami.js",
+    },
     clarity: {
       ID: "9hfzg8mbot",
     },
@@ -220,6 +220,16 @@ module.exports = {
     ],
   ],
   plugins: [
+    [
+      'docusaurus-plugin-umami',
+      {
+      },
+    ],
+    [
+      'docusaurus-plugin-clarity',
+      {
+      },
+    ],
     [
       'docusaurus-plugin-clarity',
       {
