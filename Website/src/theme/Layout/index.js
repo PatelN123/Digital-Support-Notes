c8cf88e79c7265a24cfa5760b04707c906eaccbb
codefactor-io <support@codefactor.io>
2022-04-13 08:50:37 +0000

    [CodeFactor] Apply fixes to commit 2887aae

--- Website/src/theme/Layout/index.js
+++ Website/src/theme/Layout/index.js
@@ -23,7 +23,30 @@ function Layout(props) {
   const { children, noFooter, wrapperClassName, pageClassName } = props;
   useKeyboardNavigation();
   useEffect(() => {
-    !function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");
+    !(function (w, d, i, s) {
+      function l() {
+        if (!d.getElementById(i)) {
+          var f = d.getElementsByTagName(s)[0],
+            e = d.createElement(s);
+          (e.type = 'text/javascript'),
+            (e.async = !0),
+            (e.src = 'https://canny.io/sdk.js'),
+            f.parentNode.insertBefore(e, f);
+        }
+      }
+      if ('function' != typeof w.Canny) {
+        var c = function () {
+          c.q.push(arguments);
+        };
+        (c.q = []),
+          (w.Canny = c),
+          'complete' === d.readyState
+            ? l()
+            : w.attachEvent
+            ? w.attachEvent('onload', l)
+            : w.addEventListener('load', l, !1);
+      }
+    })(window, document, 'canny-jssdk', 'script');
     Canny('initChangelog', {
       appID: '61deface8760544bd62681cd',
       position: 'bottom',
@@ -49,6 +72,5 @@ function Layout(props) {
     </LayoutProviders>
   );
 }
-
 
 export default Layout;
