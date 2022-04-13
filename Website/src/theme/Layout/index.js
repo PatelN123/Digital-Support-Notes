d2b1aa686ad2982f1ff1f3a832cf4ebc52d2ba01
Nayan Patel <naypat20051@gmail.com>
2022-04-13 07:59:50 +0000

    P1

--- Website/src/theme/Layout/index.js
+++ Website/src/theme/Layout/index.js
@@ -16,10 +16,17 @@ import LayoutHead from '@theme/LayoutHead';
 import useKeyboardNavigation from '@theme/hooks/useKeyboardNavigation';
 import { ThemeClassNames } from '@docusaurus/theme-common';
 import './styles.css';
+import { func } from 'prop-types';
 
 function Layout(props) {
   const { children, noFooter, wrapperClassName, pageClassName } = props;
   useKeyboardNavigation();
+    !function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");
+    Canny('initChangelog', {
+      appID: '61deface8760544bd62681cd',
+      position: 'bottom',
+      align: 'left',
+    });
   return (
     <LayoutProviders>
       <LayoutHead {...props} />
@@ -39,5 +46,6 @@ function Layout(props) {
     </LayoutProviders>
   );
 }
+
 
 export default Layout;
