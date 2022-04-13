919dce87193f5b7bff66f9ca10ef5d227cc9692c
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-04-13 09:54:42 +0100

    Update index.js

--- Website/src/theme/Layout/index.js
+++ Website/src/theme/Layout/index.js
@@ -4,8 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import useEffect from 'react';
+
+import React, { useEffect } from "react";
 import clsx from 'clsx';
 import SkipToContent from '@theme/SkipToContent';
 import AnnouncementBar from '@theme/AnnouncementBar';
@@ -23,30 +23,7 @@ function Layout(props) {
   const { children, noFooter, wrapperClassName, pageClassName } = props;
   useKeyboardNavigation();
   useEffect(() => {
-    !(function (w, d, i, s) {
-      function l() {
-        if (!d.getElementById(i)) {
-          var f = d.getElementsByTagName(s)[0],
-            e = d.createElement(s);
-          (e.type = 'text/javascript'),
-            (e.async = !0),
-            (e.src = 'https://canny.io/sdk.js'),
-            f.parentNode.insertBefore(e, f);
-        }
-      }
-      if ('function' != typeof w.Canny) {
-        var c = function () {
-          c.q.push(arguments);
-        };
-        (c.q = []),
-          (w.Canny = c),
-          'complete' === d.readyState
-            ? l()
-            : w.attachEvent
-            ? w.attachEvent('onload', l)
-            : w.addEventListener('load', l, !1);
-      }
-    })(window, document, 'canny-jssdk', 'script');
+    !function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");
     Canny('initChangelog', {
       appID: '61deface8760544bd62681cd',
       position: 'bottom',
@@ -72,5 +49,6 @@ function Layout(props) {
     </LayoutProviders>
   );
 }
+
 
 export default Layout;
